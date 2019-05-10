2132f814d904b5261d5c7cd9f6322ca63862ee75
GabrielPanta <gabrielpantajimenez@hotmail.com>
2019-05-10 00:27:41 -0500

    Todo ok(Validar Datos)

--- PracticaMVVM/PracticaMVVM/PracticaMVVM/Infrastructure/InstanceLocator.cs
+++ PracticaMVVM/PracticaMVVM/PracticaMVVM/Infrastructure/InstanceLocator.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+//esto es una prueba de que todo esta bien
 namespace PracticaMVVM.Infrastructure
 {
    public class InstanceLocator
